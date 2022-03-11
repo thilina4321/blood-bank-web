959591a851aeda12711c70dcff405f28f058509f
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-03-11 16:22:55 +0530

    some change

--- components/contact/TellUs.tsx
+++ components/contact/TellUs.tsx
@@ -4,7 +4,7 @@ import classes from "./tellus.module.css";
 const TellUs = () => {
   return (
     <section className={classes.section}>
-      <h1> Tell us how you're feeling</h1>
+      <h1> Tell us how your feeling</h1>
       <p>
         {" "}
         {`If you've got a suggestion, complaint or compliment, we'll see how we can put your voice to good use:
