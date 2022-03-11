3a1e7ca9735825724a4fc0f0a7037f746846a4a4
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-03-11 11:33:07 +0530

    bug fix

--- components/blood/donate-blood/Donate.tsx
+++ components/blood/donate-blood/Donate.tsx
@@ -4,10 +4,10 @@ import classes from "./donate.module.css";
 const Donate = () => {
   return (
     <section className={classes.section}>
-      <h1> I'm ready to give blood</h1>
+      <h1>{` I'm ready to give blood`} </h1>
       <div className={classes.buttons}>
-          <button className={classes.new}> I am New </button>
-          <button className={classes.before}> I Have Donate Before </button>
+        <button className={classes.new}> I am New </button>
+        <button className={classes.before}> I Have Donate Before </button>
       </div>
     </section>
   );
