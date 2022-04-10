85bf8e2238e49845ec0dfc129f3f7ae99217dc7f
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-04-10 20:44:22 +0530

    ocation icon update

--- components/contact/TellUs.tsx
+++ components/contact/TellUs.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import classes from "./tellus.module.css";
+import DialpadIcon from '@mui/icons-material/Dialpad';
 
 const TellUs = () => {
   return (
     <section className={classes.section}>
-      <h1> Tell us how your feeling</h1>
+      <h1> Tell us how your feeling   </h1>
       <p>
         {`If you've got a suggestion, complaint or compliment, we'll see how we can put your voice to good use:
 
 `}
       </p>
       <h3> Send us your suggestions </h3>
+      <h1> <DialpadIcon fontSize="large" /> </h1>
+
     </section>
   );
 };
